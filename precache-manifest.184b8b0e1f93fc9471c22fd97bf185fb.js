21df178f16e84d149b7243f115ce74e25204e3d1
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-07-11 17:47:19 -0400

    Updates

---
+++ precache-manifest.184b8b0e1f93fc9471c22fd97bf185fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acb12d59131bf7ae0c9d8c40a054fbd",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7f34479a6c5ea2d19046",
+    "url": "/my-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "137e8245d90a6d255d46",
+    "url": "/my-portfolio/static/css/main.d865d523.chunk.css"
+  },
+  {
+    "revision": "7f34479a6c5ea2d19046",
+    "url": "/my-portfolio/static/js/2.44c05a0c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-portfolio/static/js/2.44c05a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137e8245d90a6d255d46",
+    "url": "/my-portfolio/static/js/main.93685e3a.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "7cebc9b204616d7e3316d5bc4f38fc91",
+    "url": "/my-portfolio/static/media/profile.7cebc9b2.png"
+  }
+]);
